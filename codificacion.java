f1bd16e8bc5209c3b31c27590e8d63ddfeea3d7b
psycho <tazzmaniac51@gmail.com>
2016-12-09 05:05:13 +0100

    VocalesPorNumeros
    version final de vocales por números

--- codificacion.java
+++ codificacion.java
@@ -1,37 +1,36 @@
+package codificacion;
 import javax.swing.*;
+
 public class codificacion {
 
-        public static void main(String[] args) {
+	public static void main(String[] args) {
+		
+		char letra;
+		String frase,frase2="";
+		
+		frase=JOptionPane.showInputDialog("Introduce una frase:");
+		
+		for (int j = 0; j < frase.length(); j++) {
+			letra = frase.charAt(j);
+			
+			if (letra=='a') {
+				letra = '1';
+			}
+			if (letra=='e') {
+				letra = '2';
+			}
+			if (letra=='i') {
+				letra = '3';
+			}
+			if (letra=='o') {
+				letra = '4';
+			}
+			if (letra=='u') {
+				letra = '5';
+			}
+			frase2 = frase2+letra;
+		}
+	System.out.println(frase2);
+	}
 
-            char letra;
-            String frase,frase2="";
-
-            frase=JOptionPane.showInputDialog("Introduce una frase:");
-
-            for (int j = 0; j < frase.length(); j++) {
-                letra = frase.charAt(j);
-
-                if (letra=='a') {
-                    letra = 1;
-                }
-                if (letra=='e') {
-                    letra = 2;
-                }
-                if (letra=='i') {
-                    letra = 3;
-                }
-                if (letra=='o') {
-                    letra = 4;
-                }
-                if (letra=='u') {
-                    letra = 5;
-                }
-                else {
-                    frase2 = frase2+letra;
-                }
-            }
-            System.out.println(frase2);
-        }
-    }
-
-
+}
